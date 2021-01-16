bbf1df0a3db68fba77b8f3a44f211c99267daf8a
borfirbora <borafirlangec@gmail.com>
2021-01-17 02:09:02 +0300

    Updates

---
+++ precache-manifest.a077dd109dc63c6d4da70a9139f887e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e159eeace619e8aaa3b1",
+    "url": "/react-try/static/css/main.3e95d06a.chunk.css"
+  },
+  {
+    "revision": "e159eeace619e8aaa3b1",
+    "url": "/react-try/static/js/main.419a6ead.chunk.js"
+  },
+  {
+    "revision": "9c348b661c42c0c56743",
+    "url": "/react-try/static/js/runtime~main.fb025d2a.js"
+  },
+  {
+    "revision": "472f472de4ff1afb7a7c",
+    "url": "/react-try/static/js/2.65eeef47.chunk.js"
+  },
+  {
+    "revision": "ad0cc234a5879deaad69749db6b3c212",
+    "url": "/react-try/index.html"
+  }
+];
